94c71ed7c744b59a2bf5f137237f96be8abd7ade
abirremadi <abirremadi4@gmail.com>
2021-07-12 23:02:04 +0100

    add files

---
+++ models/userschema.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+const PersonSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:[true, 'please type your name !!' ]
+    },
+    age: Number,
+    favoriteFoods: [String]
+})
+module.exports = mongoose.model('Person', PersonSchema)
